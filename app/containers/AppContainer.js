2b7eee8a5c94f15f0d77d1656ae471fc6ecd3bf1
unknown <mrzqii@163.com>
2018-12-06 11:38:55 +0800

    '增加对dispach理解的一些注释'

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -8,9 +8,11 @@ import { connect } from 'react-redux'
 
 class AppContainer extends Component {
   componentDidMount() {
-    this.props.fetchPeople()
+    this.props.fetchPeopleFlag()
+    // const {dispatch, fetchPeopleFlag} = this.props
+    // dispatch(fetchPeople())
+    // alert('1:'+dispatch) // 这里可以看到dispatch的函数
   }
-
   render() {
     let content = <PeopleList people={this.props.rondomPeople.people} />;
     if (this.props.rondomPeople.isFetching) {
@@ -33,11 +35,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#093339"
   }
 });
+// 这里的返回值可以指返回自己需要的值, 通过state来进行获取
 const mapStateToProps = state => {
     return {
         rondomPeople : state
     }
 }
 
+// 通过connect以后在AppContainer组件就可以通过this.props使用了
 
-export default connect(mapStateToProps, { fetchPeople })(AppContainer)
+// 情况一: 如果已经把connect的第二个参数传入了，那么在注册的组件里面只能使用this.props.fetchPeopleFlag() 来触发action
+// 因为在props里面已经不存在dispach这个方法了，这个方法已经通过connect封装在fetchPeopleFlag里面了
+export default connect(mapStateToProps, { fetchPeopleFlag:fetchPeople })(AppContainer)
+
+// 情况二： 如果是下面的写法 那么可以直接通过import  fetchPeople 这个action，然后通过 this.props.dispatch()来触发
+// export default connect(mapStateToProps)(AppContainer)
